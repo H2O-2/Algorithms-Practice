a9ad39bf761c3e9db0507e37d6a03ded6147b84b
H2O-2 <1064737282@qq.com>
2018-03-21 22:59:41 -0700

    Assignment 1

--- part2/assignment1/src/WordNet.java
+++ part2/assignment1/src/WordNet.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 
 public class WordNet {
-    private List<String[]> synsets = new ArrayList<>();
+    private List<String> wordNetNouns = new ArrayList<>();
+    private List<List<String>> synsets = new ArrayList<>();
+    private List<List<Integer>> hypernyms = new ArrayList<>();
+    private Digraph wordNet;
+
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
@@ -19,33 +25,83 @@ public class WordNet {
         In synsetsIn = new In(synsets);
         In hypernymsIn = new In(hypernyms);
         List<Integer> ids = new ArrayList<>();
-        String nextSynset, nexthypernym;
+        String nextSynset, nextHypernym;
 
         nextSynset = synsetsIn.readLine();
-        nexthypernym = hypernymsIn.readLine();
+        nextHypernym = hypernymsIn.readLine();
 
-        while (nextSynset != null && nexthypernym != null) {
-            String[] synsetArr = nextSynset.split(",");
-            ids.add(Integer.parseInt(synsetArr[0]));
-            this.synsets.add(Arrays.copyOfRange(synsetArr, 1, synsetArr.length - 2));
+        while (nextSynset != null && nextHypernym != null) {
+            List<String> synsetList = Arrays.asList(nextSynset.split(","));
+            ids.add(Integer.parseInt(synsetList.get(0)));
+            final List<String> synset = Arrays.asList(synsetList.get(1).split(" "));
+            // add synset
+            this.synsets.add(synset);
+            this.wordNetNouns.addAll(synset);
+
+            List<String> hypernymStringList = Arrays.asList(nextHypernym.split(","));
+            List<Integer> hypernymList = new ArrayList<>();
+            for (String s : hypernymStringList) {
+                hypernymList.add(Integer.parseInt(s));
+            }
+            // remove id
+            hypernymList.remove(0);
+            this.hypernyms.add(hypernymList);
 
             nextSynset = synsetsIn.readLine();
-            nexthypernym = hypernymsIn.readLine();
+            nextHypernym = hypernymsIn.readLine();
         }
+
+        this.wordNet = new Digraph(this.synsets.size());
+
+        // initialize WordNet
+        int curId = 0;
+        for (List<Integer> hypernymIds : this.hypernyms) {
+            for (int hypernymId : hypernymIds) {
+                this.wordNet.addEdge(curId, hypernymId);
+            }
+            curId++;
+        }
+
+
+        // get index of entity (root)
+        final int rootIndex = this.synsets.indexOf(Collections.singletonList("entity"));
+        BreadthFirstDirectedPaths bfs =
+            new BreadthFirstDirectedPaths(this.wordNet.reverse(), rootIndex);
+        // debug(this.wordNet);
+        for (int n = 0; n < this.wordNet.V(); n++) {
+            if (!bfs.hasPathTo(n)) {
+                debug(n);
+                throw new IllegalArgumentException("INPUT NOT ROOTED");
+            }
+        }
+
+        debug("pass");
     }
 
     // returns all WordNet nouns
-    // public Iterable<String> nouns() {}
+    public Iterable<String> nouns() {
+        return this.wordNetNouns;
+    }
 
     // is the word a WordNet noun?
-    // public boolean isNoun(String word) {}
+    public boolean isNoun(String word) {
+        if (this.wordNetNouns.indexOf(word) < 0) {
+            return false;
+        }
+
+        return true;
+    }
 
     // distance between nounA and nounB (defined below)
-    // public int distance(String nounA, String nounB) {}
+    public int distance(String nounA, String nounB) {
+        return 0;
+    }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
-    // public String sap(String nounA, String nounB) {}
+    public String sap(String nounA, String nounB) {
+        return "";
+    }
 
     private void debug(Object o) {
         StdOut.println(o);
